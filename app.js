1092336ed2b2823ae2ff2bd3e8a7aa43aea794e0
Rahul Sharma <hubstackrealm@gmail.com>
2026-07-31 15:52:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+
+let storeInventory = [];
+// defining a class named store 
+class Store
+{
+    // creating private fields for the store which is necessary
+    #orderId
+    #customerName
+    #products 
+    #totalAmount;
+    #status;
+
+    // special function constructor to assign private fields
+    constructor(orderId, customerName, products, totalAmount, status)
+    {
+        this.#orderId = orderId;
+        this.#customerName = customerName;
+        this.#products = products;
+        this.#totalAmount = totalAmount;
+        this.#status = status;
+        // pushing the values to the array
+        storeInventory.push (this);
+        // {orderId: orderId, customerName: customerName, products: products, totalAmount: totalAmount, status: status}
+    }
+
+    // method inside the class to display all orders when forEach method is used as it will help the private fields to be accessed
+    printInventory()
+    {
+       return `Order ID: ${this.#orderId}
+                Customer: ${this.#customerName}
+                Products: ${this.#products.join(", ")}
+                Amount: ₹${this.#totalAmount}
+                Status: ${this.#status}`;
+    }
+}
+
+// creating various inventory data
+let item1 = new Store(101, "Rahul Sharma", ["Laptop", "Iphone 13"], 120000, "Delivered");
+let item2 = new Store(102, "Priyanka Sharma", ["Iphone 13"], 60000, "Pending");
+let item3 = new Store(103, "Umesh Singh", ["Laptop"], 60000, "Shipped");
+let item4 = new Store(104, "Varinder Singh", ["Plant Pot"], 10000, "Cancelled");
+
+// displaying all orders
+storeInventory.forEach(item=>
+{
+    console.log(item.printInventory());
+    console.log("--------------------")
+}
+)
